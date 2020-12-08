189a818c2a5ab561999320c3f4f8a9624de4bdf6
yyyIce <60221723+yyyIce@users.noreply.github.com>
2020-12-08 11:07:40 +0800

    Update pointer_basic.c

--- pointer_test/pointer_basic.c
+++ pointer_test/pointer_basic.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-	int c = 5;
+    int c = 5;
     int * p = &c;
     printf("c:%d	&c:%p\n", c, &c);
     printf("&p:%p	p:%p    *p:%d\n", &p, p, *p);
-	return 0; 
+    return 0; 
 }
 
